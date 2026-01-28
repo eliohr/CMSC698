54c097c9bfafc7c1d197bae9ff6c0f632c5c6b5d
Eli Hooker Reese <129206205+eliohr@users.noreply.github.com>
2026-01-27 22:24:58 -0600

    instructions

--- ConductorCamera/ConductorCamera/BTMIDIPeripheralViewController.swift
+++ ConductorCamera/ConductorCamera/BTMIDIPeripheralViewController.swift
@@ -26,7 +26,7 @@ class BTMIDIPeripheralViewController: CABTMIDILocalPeripheralViewController {
         if self.view.viewWithTag(bodyLabelTag) == nil {
             let bodyLabel = UILabel()
             bodyLabel.tag = bodyLabelTag
-            bodyLabel.text = "Turn the switch on. \n\nOn Mac open Audio MIDI Setup. In the Audio MIDI Setup menu navigate to Window > Show MIDI Studio. In the MIDI Studio menu navigate to MIDI Studio > Open Bluetooth Configuration... and connect to this iPhone. \n\nOn Windows... sorry idk but i'm sure you can find a tutorial... \n\nProceed by associating hand attributes with MIDI attributes in the menus below and tapping 'set MIDI-hand mapping'. I'll give you a little more instruction there."
+            bodyLabel.text = "Turn the switch on. \n\nOn Mac open Audio MIDI Setup. In the Audio MIDI Setup menu navigate to Window > Show MIDI Studio. In the MIDI Studio menu navigate to MIDI Studio > Open Bluetooth Configuration... and connect to this iPhone. \n\nOn Windows... sorry idk but i'm sure you can find a tutorial... \n\nProceed by closing this pane and using the settings screen to associate hand attributes with MIDI attributes in the menus below. Tap 'set MIDI-hand mapping' to apply the changes. I'll give you a little more instruction there."
             bodyLabel.numberOfLines = 0
             bodyLabel.font = UIFont.systemFont(ofSize: 15)
             bodyLabel.textColor = .secondaryLabel
